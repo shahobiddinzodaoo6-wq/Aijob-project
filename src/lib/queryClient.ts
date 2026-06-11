e12ea2dbe35d5b2f528be50738d656d28fd0d5b5
shahobiddinzodaoo6-wq <shahobiddinzoda.oo6@gmail.com>
2026-06-11 23:44:47 +0500

    edit this project++

--- src/lib/queryClient.ts
+++ src/lib/queryClient.ts
@@ -1,5 +1,6 @@
 import { QueryClient } from "@tanstack/react-query";
 
+
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
@@ -9,3 +10,4 @@ export const queryClient = new QueryClient({
     },
   },
 });
+
